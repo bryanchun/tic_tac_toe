fe424bcef4b93b1b54a38039d4175e969a5d7c16
bryanchun <chun.hiu.sang@gmail.com>
2021-03-10 23:22:04 +0800

    Add basic interaction
    - Add LiveComponent.
    - Fix bug with int/string map keys.

--- lib/tic_tac_toe_web/components/board.ex
+++ lib/tic_tac_toe_web/components/board.ex
@@ -1,18 +1,76 @@
 defmodule TicTacToeWeb.Components.Board do
-  use Surface.Component
+  use Surface.LiveComponent
+
   alias TicTacToeWeb.Components.Square
+
+  # TODO: Fix int/string conversions of the map keys
+
+  data statuses, :map,
+    default: for rowIdx <- [1, 2, 3], colIdx <- [1, 2, 3],
+      into: %{},
+      do: {{ "#{rowIdx}", "#{colIdx}"}, "playing"}
+
+  data pieces, :map,
+    default: for rowIdx <- [1, 2, 3], colIdx <- [1, 2, 3],
+      into: %{},
+      do: {{ "#{rowIdx}", "#{colIdx}" }, nil}
+
+  data whose_turn, :string, default: "X"
+
+  data winner_piece, :string, default: nil
 
   def render(assigns) do
     ~H"""
-    <div class="board-row" :for.index={{ row_idx <- [1, 2, 3] }}>
-      <span :for.index={{ col_idx <- [1, 2, 3] }}>
-        <Square
-          click="move"
-          loc_x={{ row_idx }}
-          loc_y={{ col_idx }}
-        />
-      </span>
+    <div class="container">
+      <div class="board container-col">
+        <div class="board-row" :for={{ row_idx <- [1, 2, 3] }}>
+          <span :for={{ col_idx <- [1, 2, 3] }}>
+            <Square
+              piece={{ @pieces |> Map.get({ "#{row_idx}", "#{col_idx}" }) }}
+              status={{ Map.get(@statuses, { "#{row_idx}", "#{col_idx}" }) }}
+              click="move"
+              loc_x={{ "#{row_idx}" }}
+              loc_y={{ "#{col_idx}" }}
+            />
+          </span>
+        </div>
+      </div>
+      <div class="container-col">
+        {{ next_instruction(@whose_turn, @winner_piece) }}
+      </div>
     </div>
     """
   end
+
+  defp next_instruction(whose_turn, nil), do: "Next player: #{next_turn(whose_turn)}"
+  defp next_instruction(_whose_turn, winner_piece) when winner_piece in ["O", "X"], do: "Winner: #{winner_piece}"
+  defp next_instruction(_whose_turn, " "), do: "Draw"
+
+  defp next_turn("X"), do: "O"
+  defp next_turn("O"), do: "X"
+
+  def handle_event(
+    "move",
+    %{"status" => "playing", "piece" => nil,
+      "loc_x" => loc_x, "loc_y" => loc_y},
+    socket
+  ) do
+    IO.puts("Moved { #{loc_x}, #{loc_y} }")
+
+    socket =
+      socket
+      |> update(:pieces, & Map.put(
+        &1, { loc_x, loc_y }, socket.assigns[:whose_turn]
+      ))
+      |> update(:whose_turn, &
+        next_turn(&1)
+      )
+
+    # Evaluate :winner_piece and :statuses
+
+    {:noreply, socket}
+  end
+
+  def handle_event("move", %{"status" => "won"}, socket),       do: {:noreply, socket}
+  def handle_event("move", %{"status" => "disabled"}, socket),  do: {:noreply, socket}
 end
--- lib/tic_tac_toe_web/components/square.ex
+++ lib/tic_tac_toe_web/components/square.ex
@@ -2,10 +2,13 @@ defmodule TicTacToeWeb.Components.Square do
   use Surface.Component
 
   @doc "The piece symbol on the square"
-  prop piece, :string, values: [" ", "X", "O"], default: " "
+  prop piece, :string, values: [nil, "X", "O"], default: nil
 
-  @doc "The status of this square: we will highlight it when someone won"
-  prop status, :string, values: ["playing", "won"], default: "playing"
+  @doc """
+    The status of this square: we will highlight it when someone won with this sqaure,
+    and disable it when someone didn't won with this sqaure or it's a draw
+  """
+  prop status, :string, values: ["playing", "won", "closed"], default: "playing"
 
   @doc "The event name triggered when the square gets clicked"
   prop click, :event
@@ -14,17 +17,17 @@ defmodule TicTacToeWeb.Components.Square do
     x-Location of this square on the game board. Should be 1, 2, or 3.
     To be passed to the click event as parameters.
   """
-  prop loc_x, :number, required: true
+  prop loc_x, :string, required: true
 
   @doc """
     y-Location of this square on the game board. Should be 1, 2, or 3.
     To be passed to the click event as parameters.
   """
-  prop loc_y, :number, required: true
+  prop loc_y, :string, required: true
 
   @doc "status to color for display"
-  def to_color("playing"), do: "white"
   def to_color("won"), do: "yellow"
+  def to_color(_), do: "white"
 
   def render(assigns) do
     ~H"""
@@ -32,6 +35,8 @@ defmodule TicTacToeWeb.Components.Square do
       class="square"
       style="background-color: {{ to_color(@status) }};"
       :on-click={{ @click }}
+      phx-value-piece={{ @piece }}
+      phx-value-status={{ @status }}
       phx-value-loc_x={{ @loc_x }}
       phx-value-loc_y={{ @loc_y }}
     >
